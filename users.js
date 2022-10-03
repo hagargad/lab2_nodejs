66feb6850dfe32a61852e6b01ac451ee295ff80d
hagar hosny <hagar-hosny-gad@hotmail.com>
2022-10-03 04:05:09 +0200

    Add files via upload

---
+++ users.js
@@ -0,0 +1,84 @@
+
+const express = require("express");
+
+const router = express.Router();
+
+const uuid = require("uuid");
+
+let users = require("../../Users");
+
+//Get Request 
+router.get("/", (req, res) => {
+  res.json(users);
+});
+
+//Search for user with specific id
+router.get("/:id", (req, res) => {
+  const found = users.some((user) => user.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json(users.filter((user) => user.id === parseInt(req.params.id)));
+  } else {
+    res.sendStatus(400); // Not found 
+  }
+});
+
+//post request 
+router.post("/", (req, res) => {
+  const newUser = {
+    id: uuid.v4(),
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  if (!newUser.name || !newUser.email) {
+    return res.sendStatus(400); //Cant cant add
+  }
+
+  users.push(newUser);
+
+  res.json(users);
+});
+
+// Update User
+// put request
+router.put("/:id", (req, res) => {
+  const found = users.some((user) => user.id === parseInt(req.params.id));
+
+  if (found) {
+    const updateUser = req.body;
+
+    users.forEach((user) => {
+      if (user.id === parseInt(req.params.id)) {
+        user.name = updateUser.name ? updateUser.name : user.name;
+
+        user.email = updateUser.email ? updateUser.email : user.email;
+
+        res.json({ msg: "User updated", user });
+      }
+    });
+  } else {
+    res.sendStatus(400); 
+  }
+});
+
+// Delete User
+
+router.delete("/:id", (req, res) => {
+  const found = users.some((user) => user.id === parseInt(req.params.id));
+
+  if (found) {
+    users = users.filter((user) => user.id !== parseInt(req.params.id));
+
+    res.json({
+      msg: "User deleted",
+
+      users,
+    });
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+module.exports = router;
+
